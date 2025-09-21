46e4712048148c2bf55318fbacb618f03e4cd911
khadijazain46-hub <khadijazain46@gmail.com>
2025-09-21 20:53:54 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,9 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Scalable Engineering CAD & Design By AI and Human Intelligence',
+  description: 'Scalable Engineering CAD & Design By AI and Human Intelligence',
+  generator: 'Scalable Engineering CAD & Design By AI and Human Intelligence',
 }
 
 export default function RootLayout({
