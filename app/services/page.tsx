91aa8cf67e2a3e83b5d21749486b6f94ef27fbdb
khadijazain46-hub <khadijazain46@gmail.com>
2025-09-25 21:49:20 +0530

    Update page.tsx

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -821,7 +821,7 @@ export default function ServicesPage() {
                     <p>Casper, Wyoming 82609 U.S.</p>
                   </div>
                 </div>
-                <div className="flex items-start space-x-3">
+                {/* <div className="flex items-start space-x-3">
                   <Mail className="w-5 h-5 text-[#d9d9d9] mt-0.5 flex-shrink-0" />
                   <div>
                     <p>info@krafzen.com</p>
@@ -831,7 +831,7 @@ export default function ServicesPage() {
                 <div className="flex items-center space-x-3">
                   <Phone className="w-5 h-5 text-[#d9d9d9] flex-shrink-0" />
                   <p>@</p>
-                </div>
+                </div> */}
               </div>
               <div className="mt-6">
                 <SocialIcons />
